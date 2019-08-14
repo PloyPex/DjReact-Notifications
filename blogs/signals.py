0f3f0d400efd044bdfb61d6ef3baabdc94c86476
alexHooper <alexmohack@gmail.com>
2019-08-14 20:51:02 +0530

    create blog created signal

---
+++ blogs/signals.py
@@ -0,0 +1,15 @@
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+
+from notifications.signals import notify
+
+from .models import Blog
+
+
+@receiver(post_save, sender=Blog)
+def post_save_blog_notify(sender, instance, created, *args, **kwargs):
+    if created:
+        user = instance.author
+        notify.send(user, recipient=user, verb=f"You created blog post - {instance.title}",
+            level='success')
+        # at {instance.timestamp}")
