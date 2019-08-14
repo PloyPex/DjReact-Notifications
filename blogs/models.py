a5861bf752672b3c4a9f3a746b38b280a643c55b
alexHooper <alexmohack@gmail.com>
2019-08-14 20:40:11 +0530

    create blog model

--- blogs/models.py
+++ blogs/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from django.contrib.auth import get_user_model
 
-# Create your models here.
+User = get_user_model()
+
+
+class Blog(models.Model):
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    title = models.CharField(max_length=250)
+    content = models.TextField()
+
+    def __str__(self):
+        return str(self.title)
