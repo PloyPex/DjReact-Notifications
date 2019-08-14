274955ff75102a1a977c336ad0f99010717db68e
alexHooper <alexmohack@gmail.com>
2019-08-14 21:13:01 +0530

    create notifications list component

--- frontend/src/notifications/NotificationsList.js
+++ frontend/src/notifications/NotificationsList.js
@@ -1,1 +1,26 @@
-import React, Component from 'react'
+import React, {Component} from 'react'
+
+class NotificationList extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      notifications: []
+    }
+  }
+
+  componentDidMount() {
+    const endpoint = 'http://127.0.0.1:8000/notifications/api/unread/'
+    fetch(endpoint)
+      .then(response => response)
+      .then(data => console.log(data))
+      .catch(err => console.error(err))
+  }
+
+  render() {
+    return (
+      <div>List appears here, please wait for some time.</div>
+    )
+  }
+}
+
+export default NotificationList
